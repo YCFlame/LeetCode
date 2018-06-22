74eac047335d8d93b9690644655a1f74ab40dfe7
yangchao <yangchao@douban.com>
2018-06-22 11:52:27 +0800

    performance improvement

--- Algorithms/136_Single_Number.py
+++ Algorithms/136_Single_Number.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python
 # coding: utf-8
 
+
 class Solution(object):
     # @param A, a list of integer
     # @return an integer
     def singleNumber(self, A):
+        result = A[0]
         for x in A[1:]:
-            A[0] ^= x
+            result ^= x
 
-        return A[0]
+        return result
