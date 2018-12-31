2838e78d5274fe747a5fae9a546bb688d7b1f7ee
Chao Yang <yangchao.cs@gmail.com>
2018-12-31 14:34:27 +0800

    Create 0100_Same_Tree.py

---
+++ Algorithms/0100_Same_Tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        from collections import deque
+        queue = deque([(p, q)])
+        while queue:
+            p, q = queue.popleft()
+            if p and q:
+                if p.val != q.val:
+                    return False
+                
+                queue.append((p.left, q.left))
+                queue.append((p.right, q.right))
+            else:
+                if p != q:
+                    return False
+        
+        return True
+                
