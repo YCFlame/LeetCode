26367860fb76b7dfa05a5331812d911775c7e666
Chao Yang <yangchao.cs@gmail.com>
2019-05-09 19:38:42 +0800

    Update 0066_Plus_One.py

--- Algorithms/0066_Plus_One.py
+++ Algorithms/0066_Plus_One.py
@@ -1,25 +1,11 @@
-#! /usr/bin/env python
-# coding: utf-8
-
-
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        carry = False
-        for i in range(-1, -len(digits) - 1, -1):
-            if i == -1 or carry:
-                d = digits[i] + 1
-                if d == 10:
-                    carry = True
-                    digits[i] = 0
-                else:
-                    carry = False
-                    digits[i] = d
-                    break
-        if carry:
-            digits.insert(0, 1)
-
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+        
+        digits.insert(0, 1)
         return digits
