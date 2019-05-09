8690824a9ae90d6c15ce1c6ba82ab3a82d384876
Chao Yang <yangchao.cs@gmail.com>
2019-05-09 19:45:48 +0800

    Update 0014_Longest_Common_Prefix.py

--- Algorithms/0014_Longest_Common_Prefix.py
+++ Algorithms/0014_Longest_Common_Prefix.py
@@ -1,24 +1,12 @@
 class Solution:
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        
+    def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
         
-        if len(strs) == 1:
-            return strs[0]
-        
-        common = []
-        i = 0
-        for i in range(len(strs[0])):
-            c = strs[0][i]
-            for s in strs[1:]:   
-                if i >= len(s) or s[i] != c:
-                    return ''.join(common)
-                
-            common.append(c)
-        
-        return ''.join(common)
+        count = 0
+        while True:
+            for i in range(len(strs)):
+                if count == len(strs[i]) or strs[i][count] != strs[0][count]:
+                    return strs[0][:count]
+            
+            count += 1
