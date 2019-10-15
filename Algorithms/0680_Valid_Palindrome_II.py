93d8472a83875deff771778b13a8036cd5c4a695
Chao Yang <yangchao.cs@gmail.com>
2019-10-15 21:57:01 +0800

    Update 0680_Valid_Palindrome_II.py

--- Algorithms/0680_Valid_Palindrome_II.py
+++ Algorithms/0680_Valid_Palindrome_II.py
@@ -1,16 +1,11 @@
-class Solution(object):
-    def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        start, end = 0, len(s) - 1
-        while start < end:
-            if s[start] != s[end]:
-                first, second = s[start: end], s[start + 1:end + 1]
-                return first == first[::-1] or second == second[::-1]
-            
-            start += 1
-            end -= 1
-                
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def is_palindrome(i, j):
+            return all(s[k] == s[j-k+i] for k in range(i, j))
+        
+        for i in range(len(s) // 2):         
+            if s[i] != s[~i]:
+                j = len(s) - 1 - i
+                return is_palindrome(i, j - 1) or is_palindrome(i + 1, j)
+        
         return True
