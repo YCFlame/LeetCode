2690bd68281c83fd0f8e593dd82ec16e043ae9db
Bob <yangchao.cs@gmail.com>
2019-12-10 13:35:01 +0800

    Update 0253_Meeting_Rooms_II.py

--- Algorithms/0253_Meeting_Rooms_II.py
+++ Algorithms/0253_Meeting_Rooms_II.py
@@ -27,3 +27,43 @@ class Solution:
 
         # The size of the heap tells us the minimum rooms required for all the meetings.
         return len(end_times)
+
+"""
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+
+        # If there are no meetings, we don't need any rooms.
+        if not intervals:
+            return 0
+
+        used_rooms = 0
+
+        # Separate out the start and the end timings and sort them individually.
+        start_timings = sorted([i.start for i in intervals])
+        end_timings = sorted(i.end for i in intervals)
+        L = len(intervals)
+
+        # The two pointers in the algorithm: e_ptr and s_ptr.
+        end_pointer = 0
+        start_pointer = 0
+
+        # Until all the meetings have been processed
+        while start_pointer < L:
+            # If there is a meeting that has ended by the time the meeting at `start_pointer` starts
+            if start_timings[start_pointer] >= end_timings[end_pointer]:
+                # Free up a room and increment the end_pointer.
+                used_rooms -= 1
+                end_pointer += 1
+
+            # We do this irrespective of whether a room frees up or not.
+            # If a room got free, then this used_rooms += 1 wouldn't have any effect. used_rooms would
+            # remain the same in that case. If no room was free, then this would increase used_rooms
+            used_rooms += 1    
+            start_pointer += 1   
+
+        return used_rooms
+"""
